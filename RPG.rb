5bec42f3aeb3452f7802c7a7a0a25148caccd6e0
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-03-14 18:14:14 -0300

    criando a Class Heroi.

--- RPG.rb
+++ RPG.rb
@@ -22,10 +22,36 @@ class Monstro
     end
 end
 
-monstro1 = Monstro.new
-monstro2 = Monstro.new
-puts monstro1.inspect
-puts monstro2.inspect
+class Heroi 
+    attr_accessor :energia, :ataque, :vivo, :numero_de_mortos
 
-monstro1.bater(monstro2)
-puts monstro2.inspect
+    def initialize
+        self.energia = 30
+        self.vivo = true
+        numero_de_mortos = 0
+    end
+
+    def bater(alvo)
+        if alvo.esta_vivo?
+            self.ataque = Random.rand(5) + 3
+            puts "Voce acertou o monstro, seu dano foi #{self.ataque}"
+            alvo.energia -= self.ataque
+        else
+            puts"O monstro esta morto!\n\n"
+            self.numero_de_mortos += 1
+        end
+            
+        def esta_vivo?
+            true if self.energia > 0
+        end
+    end
+end
+
+
+odin = Heroi.new
+fishman = Monstro.new
+puts odin.inspect
+puts fishman.inspect
+
+odin.bater(fishman)
+puts fishman.inspect
